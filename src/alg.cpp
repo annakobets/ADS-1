05b83cfec82b90795857b747aa9287abc4b7b386
annakobets <79173341+annakobets@users.noreply.github.com>
2021-06-06 20:20:09 +0900

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,22 +4,22 @@ int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   int l, r, mid, count, m;
   l = 0; r = size-1; count = 0;
-  mid = l+(r-l)/2;
+  mid = l + (r - l) / 2;
   while (l < r) {
     if (arr[mid] == value) {
-      count+1;
+      count += 1;
       m = mid;
       while (arr[mid] == value) {
-        count+=1;
-        mid-=1; }
+        count += 1;
+        mid -= 1; }
       mid = m;
       while (arr[mid+1] == value) {
-        count+=1;
-        mid+=1; }
+        count += 1;
+        mid += 1; }
       return count;
     } else if (arr[mid] > value) {
       r = mid;
     } else {
-      l = mid+1; }
+      l = mid + 1; }
   }
   return 0; }
